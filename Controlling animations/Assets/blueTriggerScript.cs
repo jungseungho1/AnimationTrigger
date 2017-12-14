63ccf90fe1952610fa372cc46f66a91bc2ce67ea
Seung ho Jung <jungseungho@gmail.com>
2017-12-14 15:57:46 -0500

    example v0.1
    example

---
+++ Controlling animations/Assets/blueTriggerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blueTriggerScript : MonoBehaviour {
+
+    public GameObject cube;
+    Animator anim;
+    public ParticleSystem Particlez;
+
+    void Start()
+    {
+        anim = cube.GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Blue")
+        {
+            anim.SetTrigger("makeBlue");
+            Particlez.Play();
+        }
+        if(other.gameObject.tag == "Cube")
+        {
+            anim.SetTrigger("makeShrink");
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Blue")
+        {
+            anim.SetTrigger("stopBlue");
+            Particlez.Stop();
+        }
+        if (other.gameObject.tag == "Cube")
+        {
+            anim.SetTrigger("stopShrink");
+        }
+    }
+}
